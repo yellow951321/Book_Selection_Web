99be84cfee631ff7e8a6d7c40d5aaeb7cd713daf
F74066187 <dio23310045123@gmail.com>
2019-01-12 11:20:26 +0800

    hi

--- front_end/js/edit.js
+++ front_end/js/edit.js
@@ -1,9 +1,31 @@
-const management = document.getElementById( 'page-management');
+const management = document.getElementById( 'page-management' );
+const edit = document.getElementById( 'page-edit' );
+const title = document.getElementById( 'title' );
+const addProject = document.getElementById( 'addProject' );
 
-const onclick = (event) => {
+// initialize
+edit.style.display = 'none';
+document.getElementById( 'save' ).style.display = 'none';
+const editOnclick = (event) => {
     management.style.display = 'none';
+    edit.style.display = 'block';
+    title.innerHTML = 'edit';
+
+    document.getElementById( 'save' ).style.display = 'block';
+    document.getElementById( 'add' ).style.display = 'none';
+}
+
+const addOnclick = (event) => {
+    var form = document.getElementById( 'addProjectForm' );
+    if( !form.checkValidity() ){
+    }
+    else {
+        $( '#myModal' ).modal( 'hide' )
+    }
 }
 
 Array.from(document.getElementsByClassName( 'edit' )).forEach( function(element){
-    element.addEventListener( 'click' , onclick );
+    element.addEventListener( 'click' , editOnclick );
 });
+
+addProject.addEventListener( 'click' , addOnclick );
